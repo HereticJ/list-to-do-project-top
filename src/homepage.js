68d8411adcac721525602a6baee10698e7631d84
HereticJ <HereticJ@users.noreply.github.com>
2026-09-23 16:30:22 -0700

    Rewrote several (all?) entries in index.js for better structure/clarity. Removed borders and backgrounds from buttons. Still needs mouse hover events. Set left nav button to justify-start in parent divs.

--- src/homepage.js
+++ src/homepage.js
@@ -3,59 +3,70 @@ const right = document.querySelector("#right");
 const leftAddTaskBtn = document.querySelector("#leftAddTaskBtn");
 const rightAddTaskBtn = document.querySelector("#rightAddTaskBtn");
 
-// Left pane for User details.
+// Left hand side navigation.
 export const leftPane = document.createElement("div");
-    const userProfile = document.createElement("h4");
-        userProfile.textContent = "Default User";
-    const mainLeftNav = document.createElement("ul");
-    const addTaskEntry = document.createElement("li");
-    const todayEntry = document.createElement("li");
+leftPane.id = "leftPane";
+    const userBlock = document.createElement("div");
+    userBlock.id = "userBlock";
+        const userProfile = document.createElement("h4");
+            userProfile.textContent = "Default User";
+    
+    const navBlock = document.createElement("div");
+    navBlock.id = "navBlock";
         const todayBtn = document.createElement("button");
             todayBtn.textContent = "Today";
-    const upcomingEntry = document.createElement("li");
         const upcomingBtn = document.createElement("button");
             upcomingBtn.textContent = "Upcoming";
-    const projectsLeftHeader = document.createElement("h4");
-        const projectsLeftHeaderBtn = document.createElement("button");
-            projectsLeftHeaderBtn.textContent = "My Projects";
-    const projectsLeftList = document.createElement("ul");
-        const projectLeftList1 = document.createElement("button");
-                projectLeftList1.textContent = "Fitness";
-        const projectLeftList2 = document.createElement("button");
-                projectLeftList2.textContent = "Groceries";
-        const projectLeftList3 = document.createElement("button");
-                projectLeftList3.textContent = "Appointments";
 
-// Right pane for displaying projects, tasks, and control buttons.
+    const projectBlock = document.createElement("div");
+    projectBlock.id = "projectBlock";
+            const projectsLeftHeaderBtn = document.createElement("button");
+                projectsLeftHeaderBtn.textContent = "My Projects";
+            const projectLeftBtn1 = document.createElement("button");
+            projectLeftBtn1.textContent = "Fitness";
+            const projectLeftBtn2 = document.createElement("button");
+            projectLeftBtn2.textContent = "Groceries";
+            const projectLeftBtn3 = document.createElement("button");
+                    projectLeftBtn3.textContent = "Appointments";
+
+// Initializes structure for first visit.
+left.appendChild(leftPane);
+    leftPane.appendChild(userBlock);
+        userBlock.appendChild(userProfile);
+    
+    leftPane.appendChild(navBlock);
+        navBlock.appendChild(leftAddTaskBtn);
+        navBlock.appendChild(todayBtn);
+        navBlock.appendChild(upcomingBtn);
+
+    leftPane.appendChild(projectBlock);
+        projectBlock.appendChild(projectsLeftHeaderBtn);
+            projectBlock.appendChild(projectLeftBtn1);
+            projectBlock.appendChild(projectLeftBtn2);
+            projectBlock.appendChild(projectLeftBtn3);
+
+
+
+
+
+
+
+
+
+
+            
+// Right hand side navigation.
 export const rightPane = document.createElement("div");
     const headline = document.createElement("h1");
         headline.textContent = "Today";
     const projectsRightHeader = document.createElement("h4");
         projectsRightHeader.textContent = "My Projects";
     const projectEntry = document.createElement("div");
-    
-
     const projectDetails = document.createElement("p");
-
     const dateDetails = document.createElement("p");
 
     // Exports new task button for tasks module.
     export const addNewTaskBtn = document.createElement("button");
-
-// Initializes structure for first visit.
-left.appendChild(leftPane);
-    leftPane.appendChild(userProfile);
-    leftPane.appendChild(mainLeftNav);
-        mainLeftNav.appendChild(addTaskEntry);
-        mainLeftNav.appendChild(todayEntry);
-        mainLeftNav.appendChild(upcomingEntry);
-            mainLeftNav.appendChild(upcomingBtn);
-        mainLeftNav.appendChild(projectsLeftHeader);
-            mainLeftNav.appendChild(projectsLeftHeaderBtn);
-        mainLeftNav.appendChild(projectsLeftList);
-            projectsLeftList.appendChild(projectLeftList1);
-            projectsLeftList.appendChild(projectLeftList2);
-            projectsLeftList.appendChild(projectLeftList3);
 
 right.appendChild(rightPane);
     rightPane.appendChild(headline);
